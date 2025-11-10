387211d2d6d1fa72dca48c701435e88d1f3cd631
chermie1070 <chermie.m.rilloraza@accenture.com>
2025-11-10 04:21:02 +0000

    setup django allowed hosts for codespaces

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-*%&5c+(9gy@-cixv^&+w)&+xzl^xs7_3an0nw8um!m#0ywl#bn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
@@ -87,8 +91,6 @@ DATABASES = {
             'port': 27017,
             'username': '',
             'password': '',
-            'authSource': '',
-            'authMechanism': '',
         },
     }
 }
